fdd08fe73d8648eeb90fd94227ebf8e7696af3b8
simonepri <simonepri@outlook.com>
2018-03-17 23:37:04 +0100

    Refactor implementation

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-var os = require('os')
-var stats = require('./lib/stats')
-var platform = require('./lib/platform')
-var history = {}
+'use strict'
 
-var wrapper = function (method) {
-  return function (pid, options, cb) {
-    if (typeof options === 'function') {
-      cb = options
-      options = {}
-    }
-
-    if (method === platform.UNSUPPORTED) {
-      return cb(new Error(os.platform() + ' is not supported yet, please open an issue (https://github.com/soyuka/pidusage)'), null)
-    }
-
-    options.history = history
-    return stats[method](pid, options, cb)
-  }
-}
-
-exports.stat = wrapper(platform)
-
-exports.unmonitor = function (pid) {
-  if (!pid) {
-    for (var i in history) {
-      delete history[i]
-    }
-    return
-  }
-
-  delete history[pid]
-}
-
-exports._history = history
+module.exports = require('./lib/stats')
