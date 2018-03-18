3a44c30b49796e4fcc533e92cf9ac2291edc7fbd
simonepri <simonepri@outlook.com>
2018-03-18 20:50:09 +0100

    Fix codestyle

--- test/helpers/mocks.js
+++ test/helpers/mocks.js
@@ -1,24 +1,24 @@
-import EventEmitter from 'events';
-import streamify from 'string-to-stream';
-import through from 'through';
+import EventEmitter from 'events'
+import streamify from 'string-to-stream'
+import through from 'through'
 
 module.exports = {
   spawn: (stdout, stderr, error, code, signal) => {
-    const ee = new EventEmitter();
+    const ee = new EventEmitter()
 
-    ee.stdout = through(function(d){this.queue(d)});
-    ee.stderr = through(function(d){this.queue(d)});
+    ee.stdout = through(function (d) { this.queue(d) })
+    ee.stderr = through(function (d) { this.queue(d) })
 
-    streamify(stderr).pipe(ee.stderr);
-    streamify(stdout).pipe(ee.stdout);
+    streamify(stderr).pipe(ee.stderr)
+    streamify(stdout).pipe(ee.stdout)
 
     if (error) {
-      ee.emit('error', error);
+      ee.emit('error', error)
     } else if (!stderr) {
-      ee.stdout.on('end', () => ee.emit('close', code, signal));
+      ee.stdout.on('end', () => ee.emit('close', code, signal))
     } else {
-      ee.stderr.on('end', () => ee.emit('close', code, signal));
+      ee.stderr.on('end', () => ee.emit('close', code, signal))
     }
-    return ee;
+    return ee
   }
 }
