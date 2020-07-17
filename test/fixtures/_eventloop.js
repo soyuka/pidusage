b53d45a2b5421d6b0d66ff152b5467f7d4e8ae96
soyuka <soyuka@users.noreply.github.com>
2020-07-17 09:26:52 +0200

    fix #112 normalize error code to ENOENT

--- test/fixtures/_eventloop.js
+++ test/fixtures/_eventloop.js
@@ -7,7 +7,7 @@ pidusage(process.pid, { maxage: 1500 }, function (err, stat) {
 
   console.log('Got stats', stat)
   // clean the event loop right away
-  if (process.argv[2] === '1') {
+  if (process.clear_pidusage === '1' || process.argv[2] === '1') {
     pidusage.clear()
   }
 })
