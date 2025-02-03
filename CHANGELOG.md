f5d46d12e27e9b81a0e0fd57cbf7487452eb6b5b
soyuka <soyuka@users.noreply.github.com>
2025-02-03 10:22:11 +0100

    chore: dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 ### 4.0.0
 
 - fix wmic removed on Windows 11 and add gwmi support
+- node >= 17 
 
 ### 3.0.1
 
--- test/integration.js
+++ test/integration.js
@@ -122,28 +122,28 @@ test('should throw an error if the pid is too large', async t => {
   await t.throwsAsync(async () => m(99999999))
 })
 
-test.cb('should exit right away because we cleaned up the event loop', t => {
-  if (os.platform().match(/^win/)) return t.end()
+test('should exit right away because we cleaned up the event loop', t => {
+  if (os.platform().match(/^win/)) return t.pass()
 
   process.clear_pidusage = '1'
   require(path.join(__dirname, '/fixtures/_eventloop'))
   process.nextTick(() => {
-    t.end()
+    t.pass()
   })
 })
 
-test.cb('should exit right away because the event loop ignores history', t => {
-  if (os.platform().match(/^win/)) return t.end()
+test('should exit right away because the event loop ignores history', t => {
+  if (os.platform().match(/^win/)) return t.pass()
 
   process.clear_pidusage = '0'
   require(path.join(__dirname, '/fixtures/_eventloop'))
   process.nextTick(() => {
-    t.end()
+    t.pass()
   })
 })
 
-test.cb("should use the callback if it's provided", t => {
-  m(process.pid, () => t.end())
+test("should use the callback if it's provided", t => {
+  m(process.pid, () => t.pass())
 })
 
 process.on('unhandledException', (e) => {
